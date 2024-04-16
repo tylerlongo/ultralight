e5a9342e649a4eb335e527893574ef30c41a9bac
tylerlongo <111149550+tylerlongo@users.noreply.github.com>
2024-04-16 01:46:18 -0400

    Add files via upload

---
+++ forbes109/script.js
@@ -0,0 +1,29 @@
+function updateTimeStatus() {
+    const now = new Date();
+    const hour = now.getHours();
+    let minute = now.getMinutes();
+    
+    let time = "";
+    
+    if(minute < 10) {
+        minute = "0" + minute
+    }
+    if(hour < 1) {
+        time = "current time: " + 12 + ":" + minute + "am"
+    }
+    else if(hour < 12) {
+        time = "current time: " + hour + ":" + minute + "am"
+    }
+    else if(hour < 13) {
+        time = "current time: " + hour + ":" + minute + "pm"
+    }
+    else {
+        time = "current time: " + (hour-12) + ":" + minute + "pm"
+    }
+        document.getElementById('time').innerHTML = time
+  }
+  
+  updateTimeStatus(); // Call the function to set initial status
+  
+  // Update status every second
+  setInterval(updateTimeStatus, 1000);
